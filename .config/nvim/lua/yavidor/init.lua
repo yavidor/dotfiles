fa68b58372963450af1b74ce98d5dadbfa921710
yavidor <yonatan@avidor.org>
2025-09-26 13:48:51 +0300

    Starting the nvim config

---
+++ .config/nvim/lua/yavidor/init.lua
@@ -0,0 +1,16 @@
+-- Make spacebar the leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require("yavidor.remap")
+require("yavidor.options")
+require("yavidor.lazy")
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
---
+++ .config/nvim/lua/yavidor/options.lua
@@ -0,0 +1,20 @@
+-- Options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.list = false
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.confirm = true
