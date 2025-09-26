f2e9ca455f3530ee6ee4a5fbef279d14ac256913
yavidor <yonatan@avidor.org>
2025-09-26 21:41:24 +0300

    Undotree, treesitter, telescope

---
+++ .config/nvim/lua/yavidor/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+ return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+    },
+  }
