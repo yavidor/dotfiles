920caae056893f040a6b2733f672b233a778ba13
yavidor <yonatan@avidor.org>
2026-02-17 17:45:44 +0200

    zathura

--- .config/nvim/lua/yavidor/plugins/colors.lua
+++ .config/nvim/lua/yavidor/plugins/colors.lua
@@ -6,7 +6,8 @@ return {
   },
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
+    -- vim.cmd.colorscheme 'catppuccin-mocha'
+    vim.cmd.colorscheme 'catppuccin-latte'
     vim.cmd.hi 'Comment gui=none'
     vim.api.nvim_set_hl(0, 'ColorColumn', { ctermbg = 'Red', bg = 'Red' })
   end,
