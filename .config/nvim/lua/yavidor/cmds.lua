aaaaa0f6313f45b1c54173374c3709deceb7c90d
yavidor <yonatan@avidor.org>
2026-05-24 15:06:05 +0300

    I shivin

--- .config/nvim/lua/yavidor/cmds.lua
+++ .config/nvim/lua/yavidor/cmds.lua
@@ -1,11 +1,15 @@
 vim.api.nvim_create_user_command('Gilaion', function(args)
-  vim.api.nvim_command '%s/#\\+\\s\\+\\(.\\+\\)/\\\\section*{\\1}/'
+  vim.api.nvim_command '%s/###\\s\\+\\(.\\+\\)/\\\\subsection*{\\1}/e'
 
-  vim.api.nvim_command '%s/^\\$\\$\\n\\\\begin{gather}/\\\\begin{gather*}'
+  vim.api.nvim_command '%s/##\\s\\+\\(.\\+\\)/\\\\subsubsection*{\\1}/e'
 
-  vim.api.nvim_command '%s/^\\\\end{gather}\\n\\$\\$/\\\\end{gather*}'
+  vim.api.nvim_command '%s/#\\+\\s\\+\\(.\\+\\)/\\\\section*{\\1}/e'
 
-  vim.api.nvim_command '%s/^\\$\\$\\n\\\\begin{cases}/\\\\begin{equation*}\\r\\\\begin{cases}'
+  vim.api.nvim_command '%s/^\\$\\$\\n\\s\\+\\\\begin{gather}/\\\\begin{gather*}/e'
 
-  vim.api.nvim_command '%s/\\\\end{cases}\\n\\$\\$/\\\\end{cases}\\r\\\\end{equation*}'
+  vim.api.nvim_command '%s/^\\s\\+\\\\end{gather}\\n\\$\\$/\\\\end{gather*}/e'
+
+  vim.api.nvim_command '%s/^\\$\\$\\n\\\\begin{cases}/\\\\begin{equation*}\\r\\\\begin{cases}/e'
+
+  vim.api.nvim_command '%s/\\\\end{cases}\\n\\$\\$/\\\\end{cases}\\r\\\\end{equation*}/e'
 end, {})
