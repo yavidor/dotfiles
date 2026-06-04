65119680d331ac227d2099911e88986ea90d7129
yavidor <yonatan@avidor.org>
2026-06-04 19:10:22 +0300

    No formatting cpp for now

--- .config/nvim/lua/yavidor/plugins/formatting.lua
+++ .config/nvim/lua/yavidor/plugins/formatting.lua
@@ -19,8 +19,8 @@ return { -- Autoformat
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      -- local disable_filetypes = { c = false, cpp = false }
-      local disable_filetypes = {}
+      local disable_filetypes = { c = false, cpp = true }
+      -- local disable_filetypes = {}
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
       else
